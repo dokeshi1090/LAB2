3630d5f367f944baccfd19748d2c25498795969d
Josh <javierjosh1090@icloud.com>
2026-07-13 14:02:42 +0700

    7

--- Lab2/src/components/EventCard.vue
+++ Lab2/src/components/EventCard.vue
@@ -1,34 +1,46 @@
 <script setup lang="ts">
+import { ref } from 'vue' 
 // defineProps<{
 //   msg: string
 // }>()
+const event = ref({
+  id: 5928101,
+  category: 'animal welfare',
+  title: 'Cat Adoption Day',
+  description: 'Find your new feline friend at this event.',
+  location: 'Meow Town',
+  date: 'January 28, 2026',
+  time: '12:00',
+  petsAllowed: true,
+  organizer: 'Kat Laydee'
+})
 </script>
 
 <template>
-  <div class="greetings"></div>
+  <div class="event-class">
+    <div class="event-card">
+      <h2>{{ event.title }}</h2>
+      <span>@{{ event.time }} on {{ event.date }}</span>
+    </div>
+  </div>
 </template>
 
+
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
+.event-class {
+  display: flex;
+  justify-content: center;
 }
-
-h3 {
-  font-size: 1.2rem;
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
 }
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
 }
 </style>
+

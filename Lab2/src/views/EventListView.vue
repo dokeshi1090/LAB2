ca90cf0742ecf02486f301693ccc455efbf926ae
Josh <javierjosh1090@icloud.com>
2026-07-13 15:48:38 +0700

    16.3

--- Lab2/src/views/EventListView.vue
+++ Lab2/src/views/EventListView.vue
@@ -1,19 +1,28 @@
 <script setup lang="ts">
   import EventCard from '@/components/EventCard.vue'
   import Categories from '@/components/Categories.vue'
-  import type { Event } from '@/types'
+  import type { Event, Student } from '@/types'
   import { ref, onMounted } from 'vue'
   import EventService from '@/services/EventService'
+  import StudentService from '@/services/StudentService'
 
 const events = ref<Event[] | null>(null)
+const students = ref<Student[] | null>(null)
 
 onMounted(() => {
    EventService.getEvents()
     .then ((response) => {
       events.value = response.data
     })
-
 })
+
+onMounted(() => {
+   StudentService.getStudents()
+    .then((response) => {
+      students.value = response.data
+    })
+})
+
 </script>
 
 
@@ -24,6 +33,14 @@ onMounted(() => {
     <EventCard v-for="event in events" :key="event.id" :event="event" />
     <Categories v-for="event in events" :key="'cat-' + event.id" :event="event" />
   </div>
+
+  <h1>Students</h1>
+  <div class="students">
+    <div v-for="student in students" :key="student.id" class="student-card">
+      <p><b>{{ student.name }} {{ student.surname }}</b></p>
+      <p>GPA: {{ student.gpa }}</p>
+    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -32,6 +49,26 @@ onMounted(() => {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.students {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
+}
+
+.student-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+
+.student-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
 }
 
 </style>
